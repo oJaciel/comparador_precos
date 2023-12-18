555e349d1db658f785a05778d9db0e43dfa2a292
oJaciel <149974137+oJaciel@users.noreply.github.com>
2023-12-17 22:07:39 -0300

    Atualização de banco de dados

--- database/database.sql
+++ database/database.sql
@@ -1,15 +1,19 @@
 CREATE TABLE Categoria (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(80) NOT NULL
+    nome VARCHAR(80) NOT NULL,
+		criado_em TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    atualizado_em TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Produto (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(80) NOT NULL,
     marca VARCHAR(60) NOT NULL,
-    peso VARCHAR(20) NOT NULL, 
-    imagem VARCHAR(255),
-    categoria_id BIGINT NOT NULL, 
+    peso VARCHAR(20) NOT NULL,
+    imagem VARCHAR(500),
+    categoria_id BIGINT NOT NULL,
+		criado_em TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    atualizado_em TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP, 
 
     FOREIGN KEY (categoria_id) REFERENCES Categoria(id)
 );
@@ -17,14 +21,18 @@ CREATE TABLE Produto (
 CREATE TABLE Empresa (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(150) NOT NULL,
-    imagem_logo VARCHAR(255), 
-    imagem_empresa VARCHAR(255) 
+    imagem_logo VARCHAR(500), 
+    imagem_empresa VARCHAR(500),
+		criado_em TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    atualizado_em TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Preco (
     preco FLOAT NOT NULL,
-    produto_id BIGINT NOT NULL,
+    produto_id BIGINT NOT NULL, 
     empresa_id BIGINT NOT NULL,
+		criado_em TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    atualizado_em TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
 
     FOREIGN KEY (produto_id) REFERENCES Produto(id),
     FOREIGN KEY (empresa_id) REFERENCES Empresa(id)
